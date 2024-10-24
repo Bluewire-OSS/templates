814fd942dd48b7053baba52ba00dc603809d7089
patricktbp <135556230+n1d3v@users.noreply.github.com>
2024-10-24 19:23:47 +0100

    Add pl-PL translations (WIP)

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/pl')
+def polska():
+    return render_template('index-pl.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
