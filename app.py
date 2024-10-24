df7d838b4fa2fedfe13ac049856d16efd36b76ea
patricktbp <135556230+n1d3v@users.noreply.github.com>
2024-10-24 20:58:58 +0100

    Add /ua to app.py and fix a bit in index-pl.html and CSS styling

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ def index():
     return render_template('index.html')
 
 @app.route('/pl')
-def polska():
+def polish():
     return render_template('index-pl.html')
+
+@app.route('/ua')
+def ukrainian():
+    return render_template('index-ua.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
