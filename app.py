d611a95224542ae1c878dc0eb7bee76d779e880b
patricktbp <135556230+n1d3v@users.noreply.github.com>
2024-10-25 16:13:15 +0100

    Clean up app.py

--- app.py
+++ app.py
@@ -6,29 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/pl')
-def polish():
-    return render_template('index-pl.html')
-
-@app.route('/ua')
-def ukrainian():
-    return render_template('index-ua.html')
-
-@app.route('/ro')
-def romanian():
-    return render_template('index-ro.html')
-
-@app.route('/de')
-def germany():
-    return render_template('index-de.html')
-
-@app.route('/es')
-def spanish():
-    return render_template('index-es.html')
-
-@app.route('/hr')
-def croatian():
-    return render_template('index-hr.html')
+@app.route('/<lang_code>')
+def language(lang_code):
+    supported_languages = ['pl', 'ua', 'ro', 'de', 'es', 'hr']
+    if lang_code in supported_languages:
+        return render_template(f'index-{lang_code}.html')
+    return render_template('languages.html')
 
 @app.route('/languages')
 def languages():
